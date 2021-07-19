e39d4ce84d3dc15d88d695bd2e5abdc45284a309
Omar Khan <omar@webeginswith.me>
2021-07-19 11:50:53 -0700

    Initial Commit

---
+++ day 6 mini project 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  day 6 mini project 3
+//
+//  Created by Omar on 2021-07-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
